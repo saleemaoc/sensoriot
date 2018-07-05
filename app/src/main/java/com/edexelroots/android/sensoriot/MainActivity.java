a71da3ebe994d5d41f83ba5b35a90becf553274d
Saleem Ullah <saleem.ullah.88@gmail.com>
2018-07-05 15:45:43 +0500

    commit

--- app/src/main/java/com/edexelroots/android/sensoriot/MainActivity.java
+++ app/src/main/java/com/edexelroots/android/sensoriot/MainActivity.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 
+import com.ubhave.sensormanager.ESException;
+import com.ubhave.sensormanager.ESSensorManager;
+import com.ubhave.sensormanager.SensorDataListener;
+import com.ubhave.sensormanager.data.SensorData;
+import com.ubhave.sensormanager.sensors.SensorUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -21,12 +27,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    protected void checkSelected(View v) {
+    public void checkSelected(View v) {
         // TODO - Add implementation for reading respective sensors data
 
         switch (v.getId()) {
             case R.id.check_accel:
                 // read accelerometer data
+                subscribeToAccelerometer(v.isSelected());
                 break;
             case R.id.check_gyro:
                 // read gyro data
@@ -42,6 +49,50 @@ public class MainActivity extends AppCompatActivity {
             default:
                 Utils.logE(getClass().getName(), "switch default shouldn't have occurred!!");
                 break;
+        }
+    }
+
+//    ArrayList<Integer> subIDs = new ArrayList<>();
+
+    int accelerometerId = -1;
+    protected void subscribeToAccelerometer(boolean start) {
+        try {
+            ESSensorManager sm = ESSensorManager.getSensorManager(this);
+            if(start) {
+                // start sensing
+                accelerometerId = sm.subscribeToSensorData(SensorUtils.SENSOR_TYPE_ACCELEROMETER, new AccelerometerListener());
+            } else if(accelerometerId != -1){
+                // stop sensing
+                sm.unsubscribeFromSensorData(accelerometerId);
+            }
+        } catch (ESException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        try {
+            ESSensorManager sm = ESSensorManager.getSensorManager(this);
+            Utils.logE(getClass().getName(), "Unsubing sensors");
+            sm.unsubscribeFromSensorData(accelerometerId);
+        } catch (ESException e) {
+            e.printStackTrace();
+        }
+        super.onDestroy();
+    }
+
+    private class AccelerometerListener implements SensorDataListener {
+
+        @Override
+        public void onDataSensed(SensorData sensorData) {
+            String str = sensorData.getSensorType() + " " + sensorData.getTimestamp();
+            Utils.logE(getClass().getName(), str);
+        }
+
+        @Override
+        public void onCrossingLowBatteryThreshold(boolean b) {
+
         }
     }
 }
