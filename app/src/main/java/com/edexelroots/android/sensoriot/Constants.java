0786003a3c79bb946af8fc32cbeb313f5b34de1e
Saleem Ullah <saleem.ullah.88@gmail.com>
2018-11-05 12:09:58 +0500

    push to remote

--- app/src/main/java/com/edexelroots/android/sensoriot/Constants.java
+++ app/src/main/java/com/edexelroots/android/sensoriot/Constants.java
@@ -1,4 +1,13 @@
 package com.edexelroots.android.sensoriot;
 
 public class Constants {
+
+    public static class Rekognition {
+        public static String streamProcessorName="streamProcessorForCam";
+        public static String kinesisVideoStreamArn="arn:aws:kinesisvideo:ap-northeast-1:791818220004:stream/liverekprototype/*";
+        public static String kinesisDataStreamArn="arn:aws:kinesis:ap-northeast-1:791818220004:stream/liverekprototype_data";
+        public static String roleArn="arn:aws:iam::791818220004:role/liverekprototypeKinesisRek";
+        public static String collectionId="aws:rekognition:ap-northeast-1:791818220004:collection/linkedin";
+        public static Float matchThreshold=88F;
+    }
 }
